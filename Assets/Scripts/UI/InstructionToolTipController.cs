8096527e7f3feb2fe4fd115f1077af2b9b9d6ecf
rfig1226 <rosa.fig@outlook.com>
2025-04-21 11:01:36 -0400

    better UI panels with functionality

---
+++ Assets/Scripts/UI/InstructionToolTipController.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+
+public class InstructionToolbar : MonoBehaviour
+{
+    public RectTransform panelRect;
+    public TextMeshProUGUI instructionText;
+
+    public float slideDuration = 0.4f;
+    public float slideOffset = 200f;
+
+    private Vector2 originalPos;
+    private Coroutine currentAnim;
+
+    private string[] testInstructions = {
+    "Go to the fridge",
+    "Grab the lettuce",
+    "Toast the bun",
+    "Assemble the burger"
+};
+
+    private int currentInstruction = 0;
+
+    void Awake()
+    {
+        originalPos = panelRect.anchoredPosition;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(TestInstructionLoop());
+    }
+
+
+    /*
+    * Shows new current instruction string and animates new instruction into scene
+    */
+    public void ShowInstruction(string instruction)
+    {
+        gameObject.SetActive(true);
+        panelRect.anchoredPosition = new Vector2(originalPos.x, originalPos.y + slideOffset);
+        instructionText.text = instruction;
+
+        if (currentAnim != null) StopCoroutine(currentAnim);
+        currentAnim = StartCoroutine(SlideIn());
+    }
+
+    public void HideInstruction()
+    {
+        if (currentAnim != null) StopCoroutine(currentAnim);
+        currentAnim = StartCoroutine(SlideOut());
+    }
+
+    /* Helpers */
+
+    private IEnumerator SlideIn()
+    {
+        Vector2 startPos = new Vector2(originalPos.x, originalPos.y + slideOffset);
+        Vector2 endPos = originalPos;
+
+        float t = 0f;
+        panelRect.anchoredPosition = startPos;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime / slideDuration;
+            panelRect.anchoredPosition = Vector2.Lerp(startPos, endPos, Mathf.SmoothStep(0, 1, t));
+            yield return null;
+        }
+
+        panelRect.anchoredPosition = endPos;
+    }
+
+    private IEnumerator SlideOut()
+    {
+        Vector2 startPos = panelRect.anchoredPosition;
+        Vector2 endPos = new Vector2(originalPos.x, originalPos.y + slideOffset);
+
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime / slideDuration;
+            panelRect.anchoredPosition = Vector2.Lerp(startPos, endPos, Mathf.SmoothStep(0, 1, t));
+            yield return null;
+        }
+
+        panelRect.anchoredPosition = endPos;
+    }
+    private IEnumerator TestInstructionLoop()
+    {
+        while (true)
+        {
+            ShowInstruction(testInstructions[currentInstruction]);
+            currentInstruction = (currentInstruction + 1) % testInstructions.Length;
+
+            yield return new WaitForSeconds(5f);
+
+            HideInstruction();
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
--- Assets/Scripts/UI/ProgressBar.cs
+++ Assets/Scripts/UI/ProgressBar.cs
@@ -1,18 +1,69 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class ProgressBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Image ProgressBar;
+    [SerializeField]
+    private float animationSpeed = 1f;
+    [SerializeField]
+    private UnityEvent<float> OnProgress;
+    [SerializeField]
+    private UnityEvent OnCompleted;
+
+    private Coroutine AnimationCoroutine;
+
+    private void Start()
     {
-        
+        StartCoroutine(TestProgressLoop());
     }
 
-    // Update is called once per frame
-    void Update()
+    /*
+    * Set progress bar fill to represent recipe progress by giving float >0 and <1
+    */
+    public void SetProgress(float targetProgress)
     {
-        
+
+        if (targetProgress != ProgressBar.fillAmount)
+        {
+            if (AnimationCoroutine != null)
+            {
+                StopCoroutine(AnimationCoroutine);
+            }
+
+            AnimationCoroutine = StartCoroutine(AnimateProgress(targetProgress));
+        }
+    }
+
+    private IEnumerator AnimateProgress(float targetProgress)
+    {
+        float time = 0;
+        float startProgress = ProgressBar.fillAmount;
+
+        while (time < 1)
+        {
+            ProgressBar.fillAmount = Mathf.Lerp(startProgress, targetProgress, time);
+            time += Time.deltaTime * animationSpeed;
+
+            OnProgress?.Invoke(ProgressBar.fillAmount);
+            yield return null;
+        }
+
+        ProgressBar.fillAmount = targetProgress;
+        OnProgress?.Invoke(targetProgress);
+        OnCompleted?.Invoke();
+    }
+
+    private IEnumerator TestProgressLoop()
+    {
+        while (true)
+        {
+            float randomProgress = Random.Range(0.2f, 1f);
+            SetProgress(randomProgress);
+            yield return new WaitForSeconds(5f);
+        }
     }
 }
