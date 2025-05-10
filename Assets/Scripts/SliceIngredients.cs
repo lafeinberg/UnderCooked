8a22682a3f0394cfef16acdd844c095f29d7f6ff
Ethan Miller <ethanmiller@MacBook-Pro-1176.local>
2025-05-09 20:12:20 -0400

    kind of game flow, did a lot with wayfinding, need to fix proxy

--- Assets/Scripts/SliceIngredients.cs
+++ Assets/Scripts/SliceIngredients.cs
@@ -3,34 +3,44 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class SliceIngredients : MonoBehaviour
 {
-    [SerializeField] private GameObject knifeObject;
     [SerializeField] private GameObject slicedTomatoPrefab;
     [SerializeField] private int hitsToSlice = 3;
-    [Tooltip("Particle system to play on each registered cut")]
-    [SerializeField] private ParticleSystem cutParticles;
+    [SerializeField] private ParticleSystem cutParticlesPrefab;
 
     private int hitCount = 0;
     private bool isCollidingWithKnife = false;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject == knifeObject && !isCollidingWithKnife)
-        {
-            isCollidingWithKnife = true;
-            hitCount++;
+        if (!collision.gameObject.CompareTag("Knife") || isCollidingWithKnife)
+            return;
 
-            if (cutParticles != null)
-                cutParticles.Play();
+        isCollidingWithKnife = true;
+        hitCount++;
 
-            if (hitCount >= hitsToSlice)
-                SliceTomato();
-        }
+        if (cutParticlesPrefab != null)
+            SpawnCutParticles(collision.GetContact(0));
+
+        if (hitCount >= hitsToSlice)
+            SliceTomato();
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject == knifeObject)
+        if (collision.gameObject.CompareTag("Knife"))
             isCollidingWithKnife = false;
+    }
+
+    private void SpawnCutParticles(ContactPoint contact)
+    {
+        var ps = Instantiate(
+            cutParticlesPrefab,
+            contact.point,
+            Quaternion.LookRotation(contact.normal)
+        );
+
+        ps.Play();
+        Destroy(ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
     }
 
     private void SliceTomato()
@@ -41,7 +51,6 @@ public class SliceIngredients : MonoBehaviour
             transform.rotation,
             transform.parent
         );
-
         Destroy(gameObject);
     }
 }
